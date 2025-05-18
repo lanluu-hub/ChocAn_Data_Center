7974a76f7661a408ac6b6c52e373c273ec2c36ad
Lan Quy Luu <luuquylan.keil@gmail.com>
2025-05-17 18:12:09 -0700

    Updated ChocAnSystem.h/.cpp, added ChocAnSystem to makefile

---
+++ src/ChocAnSystem.h
@@ -0,0 +1,38 @@
+/*
+ * Created: May 17, 2025
+ *----------------------------- 
+ * ChocAnSystem.h
+ * 
+ * This file defines the ChocAnSystem class, which serves as the core controller
+ * for the ChocAn Data Processing System. It provides centralized access to 
+ * business logic and system operations such as member validation, service logging, 
+ * report generation, and coordination with the Database subsystem.
+ *
+ * Part of the Team Whitespace ChocAn Data Processing System – CS314 Project
+ */
+#ifndef CHOCAN_SYSTEM_H
+#define CHOCAN_SYSTEM_H
+#include <iostream>
+
+class ChocAnSystem {
+    public:
+        // delete Copy constructor to prevent copy
+        ChocAnSystem(const ChocAnSystem& obj) = delete;
+        
+        // Returns the thread-safe, lazily-initialized Singleton instance.
+        // This ensures only one shared ChocAnSystem exists across the application. 
+        // Usage:
+        //     ChocAnSystem::getInstance().validateMember(...);
+        //     ChocAnSystem::getInstance().billService(...);
+        // Syntax: ChocAnSystem::getInstance().[Public_Function_Here]();
+        static ChocAnSystem& getInstance();
+
+        // Other Public Fucntion here:
+        void test_output() {std::cout << "Hello World!" << std::endl;}
+
+    private:
+        // Private Constructor
+        ChocAnSystem();
+};
+
+#endif
