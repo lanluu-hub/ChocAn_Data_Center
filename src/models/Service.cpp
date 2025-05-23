73bda32ec541f6665b1797d4dc42caac94ced72e
Lan Quy Luu <luuquylan.keil@gmail.com>
2025-05-23 14:41:41 -0700

    Update models for enhancement

--- src/models/Service.cpp
+++ src/models/Service.cpp
@@ -1,10 +1,10 @@
 #include "Service.h"
 
-Service::Service(std::string s_code, std::string s_name, float s_fee)
+Service::Service(const std::string& s_code, const std::string& s_name, const float s_fee):
+    serviceCode(s_code.substr(0,6))
+    , serviceName(s_name.substr(0, 20))
+    , serviceFee(s_fee) 
 {
-    serviceCode = s_code.substr(0, 6);
-    serviceName = s_name.substr(0, 20);
-    serviceFee = s_fee;
 }
 
 Service::~Service()
